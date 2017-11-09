ad89af5eecae17efd37750a477b97cb9f573e91e
Anri Asaturov <anri82@gmail.com>
2017-11-10 01:41:28 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+cd ..
 git clone https://github.com/PeerioTechnologies/peerio-desktop.git
 cd peerio-desktop
+./scripts/install-win-build-tools.sh
 ./scripts/deploy-futurio.sh
