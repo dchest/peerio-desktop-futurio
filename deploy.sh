3f84bb6a5d04c7e4bfba305ec7b48a18a4283de6
Anri Asaturov <anri82@gmail.com>
2017-11-10 01:20:45 +0200

    deploy script simplified

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,4 @@
 
 git clone https://github.com/PeerioTechnologies/peerio-desktop.git
 cd peerio-desktop
-npm install
-
-set -e
-export NODE_ENV=production
-
-./node_modules/.bin/standard-version
-git tag -d `git describe --tags` # Remove latest tag created by standard-version
-git push && git push --follow-tags origin master
-
-peerio-desktop-release --repository PeerioTechnologies/peerio-desktop \
-                       --overrides PeerioTechnologies/peerio-desktop-futurio \
-                       --publish \
-                       --tag dev \
-                       --nosign
+./scripts/deploy-futurio.sh
