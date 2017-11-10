e77b6566a692ad1bce758fb03d55fd4773477f4d
Anri Asaturov <anri82@gmail.com>
2017-11-10 07:19:52 +0200

    fix ci

--- prepare.sh
+++ prepare.sh
@@ -3,3 +3,9 @@ cd ..
 git clone https://github.com/PeerioTechnologies/peerio-desktop.git
 cd peerio-desktop
 ./scripts/install-win-build-tools.sh
+brew link wine
+brew link mono
+brew link gnu-tar
+brew link libicns
+brew link graphicsmagick
+brew link xz
